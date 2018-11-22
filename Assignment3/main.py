5022899a17962e15bf5b1e10926e10680f1a44de
Temur <tim_1998@bk.ru>
2018-11-22 23:27:59 +0300

    Rename MySqlConnection 'db' to 'conn'

--- Assignment3/main.py
+++ Assignment3/main.py
@@ -2,10 +2,10 @@ import mysql.connector
 
 from create_tables import create_database
 
-db = mysql.connector.connect(
+conn = mysql.connector.connect(
     host="localhost",
     user="root",
     passwd="test"
 )
 
-create_database(db)
+create_database(conn)
