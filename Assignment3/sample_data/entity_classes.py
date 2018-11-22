a376b53f749bf74e6d4ee69d33d6495040a3f68b
Temur <tim_1998@bk.ru>
2018-11-22 23:28:49 +0300

    Rename MySqlConnection 'db' to 'conn', add minor changes

--- Assignment3/sample_data/entity_classes.py
+++ Assignment3/sample_data/entity_classes.py
@@ -29,15 +29,15 @@ class Customer:
         self.address = fake.address()
         self.nearest_station = nearest_station
 
-    def save(self, db: MySQLConnection):
-        cursor = db.cursor()
+    def save(self, conn: MySQLConnection):
+        cursor = conn.cursor()
         sql = "INSERT INTO customers " \
               "(full_name, phone_number, email, bank_account, username, gps_location, address, nearest_station)" \
               "VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
         val = (self.name, self.phone_number, self.email, self.bank_account,
                self.username, self.gps_location, self.address, self.nearest_station)
         cursor.execute(sql, val)
-        db.commit()
+        conn.commit()
         self.id = cursor.lastrowid
 
 
@@ -46,14 +46,14 @@ class Deposit:
         self.id = None
         self.bank_account = fake.credit_car_number()
 
-    def save(self, db: MySQLConnection):
-        cursor = db.cursor()
+    def save(self, conn: MySQLConnection):
+        cursor = conn.cursor()
         sql = "INSERT INTO deposits " \
               "(bank_account) " \
               "VALUES (%s)"
         val = (self.bank_account,)
         cursor.execute(sql, val)
-        db.commit()
+        conn.commit()
         self.id = cursor.lastrowid
 
 
@@ -64,14 +64,14 @@ class CarModel:
         self.charging_capacity = randint(100, 300)
         self.plug = plug
 
-    def save(self, db: MySQLConnection):
-        cursor = db.cursor()
+    def save(self, conn: MySQLConnection):
+        cursor = conn.cursor()
         sql = "INSERT INTO car_models " \
               "(model, rent_price, charging_capacity, pmodel) " \
               "VALUES (%s, %s, %s, %s)"
         val = (self.model, self.rent_price, self.charging_capacity, self.plug.model)
         cursor.execute(sql, val)
-        db.commit()
+        conn.commit()
 
 
 class Car:
@@ -80,14 +80,14 @@ class Car:
         self.car_model = car_model
         self.color = fake.safe_color_name()
 
-    def save(self, db: MySQLConnection):
-        cursor = db.cursor()
+    def save(self, conn: MySQLConnection):
+        cursor = conn.cursor()
         sql = "INSERT INTO cars " \
               "(plate, cmodel, color) " \
               "VALUES (%s, %s, %s)"
         val = (self.plate, self.car_model, self.color)
         cursor.execute(sql, val)
-        db.commit()
+        conn.commit()
 
 
 class ChargingStation:
@@ -97,14 +97,14 @@ class ChargingStation:
         self.price_per_amount = randint(5, 50)
         self.total_no_of_sockets = randint(1, 25)
 
-    def save(self, db: MySQLConnection):
-        cursor = db.cursor()
+    def save(self, conn: MySQLConnection):
+        cursor = conn.cursor()
         sql = "INSERT INTO charging_stations " \
               "(gps_location, price_per_amount, total_no_of_sockets) " \
               "VALUES (%s, %s, %s)"
         val = (self.gps_location, self.price_per_amount, self.total_no_of_sockets)
         cursor.execute(sql, val)
-        db.commit()
+        conn.commit()
         self.id = cursor.lastrowid
 
 
@@ -115,14 +115,14 @@ class Plug:
         self.size = fake.random_digit_not_null()
         self.charging_speed = randint(5, 10)
 
-    def save(self, db: MySQLConnection):
-        cursor = db.cursor()
+    def save(self, conn: MySQLConnection):
+        cursor = conn.cursor()
         sql = "INSERT INTO plugs " \
               "(model, shape, size, charging_speed) " \
               "VALUES (%s, %s, %s, %s)"
         val = (self.model, self.shape, self.size, self.charging_speed)
         cursor.execute(sql, val)
-        db.commit()
+        conn.commit()
 
 
 class Workshop:
@@ -131,14 +131,14 @@ class Workshop:
         self.location = fake.address()
         self.available_timing = randint(0, 24)
 
-    def save(self, db: MySQLConnection):
-        cursor = db.cursor()
+    def save(self, conn: MySQLConnection):
+        cursor = conn.cursor()
         sql = "INSERT INTO workshops " \
               "(location, available_timing) " \
               "VALUES (%s, %s)"
         val = (self.location, self.available_timing)
         cursor.execute(sql, val)
-        db.commit()
+        conn.commit()
         self.id = cursor.lastrowid
 
 
@@ -150,14 +150,14 @@ class Provider:
         self.phone_number = fake.phone_number()
         self.bank_account = fake.credit_car_number()
 
-    def save(self, db: MySQLConnection):
-        cursor = db.cursor()
+    def save(self, conn: MySQLConnection):
+        cursor = conn.cursor()
         sql = "INSERT INTO providers " \
               "(name, address, phone_number, bank_account) " \
               "VALUES (%s, %s, %s, %s)"
         val = (self.name, self.address, self.phone_number, self.bank_account)
         cursor.execute(sql, val)
-        db.commit()
+        conn.commit()
         self.id = cursor.lastrowid
 
 
@@ -168,14 +168,14 @@ class CarPart:
         self.type = fake.random_element(elements=tuple(get_car_part_names()))
         self.price = randint(25, 500)
 
-    def save(self, db: MySQLConnection):
-        cursor = db.cursor()
+    def save(self, conn: MySQLConnection):
+        cursor = conn.cursor()
         sql = "INSERT INTO car_parts " \
               "(trade_name, pid, type, price) " \
               "VALUES (%s, %s, %s, %s)"
         val = (self.trade_name, self.provider.id, self.type, self.price)
         cursor.execute(sql, val)
-        db.commit()
+        conn.commit()
 
 
 class Order:
@@ -186,14 +186,14 @@ class Order:
         self.date_time = get_fake_date_time(fake)
         self.payment = payment
 
-    def save(self, db: MySQLConnection):
-        cursor = db.cursor()
+    def save(self, conn: MySQLConnection):
+        cursor = conn.cursor()
         sql = "INSERT INTO orders " \
               "(wid, pid, date_time, no_of_transaction) " \
               "VALUES (%s, %s, '%s', %s)"
         val = (self.workshop.id, self.provider.id, self.date_time, self.payment.transaction)
         cursor.execute(sql, val)  # TODO insertion of datetime is to be checked
-        db.commit()
+        conn.commit()
         self.id = cursor.lastrowid
 
 
@@ -206,14 +206,14 @@ class RentRecord:
         self.from_date_time = get_fake_date_time(fake)
         self.to_date_time = get_fake_date_time(fake)
 
-    def save(self, db: MySQLConnection):
-        cursor = db.cursor()
+    def save(self, conn: MySQLConnection):
+        cursor = conn.cursor()
         sql = "INSERT INTO rent_records " \
               "(cid, cplate, distance, date_from, date_to) " \
               "VALUES (%s, %s, %s, '%s', '%s')"
         val = (self.customer.id, self.car.plate, self.distance, self.from_date_time, self.to_date_time)
         cursor.execute(sql, val)
-        db.commit()
+        conn.commit()
         self.id = cursor.lastrowid
 
 
@@ -225,14 +225,14 @@ class ChargingRecord:
         self.price = charging_station.price_per_amount * car.charging_capacity
         self.date_time = get_fake_date_time(fake)
 
-    def save(self, db: MySQLConnection):
-        cursor = db.cursor()
+    def save(self, conn: MySQLConnection):
+        cursor = conn.cursor()
         sql = "INSERT INTO charge_records " \
               "(sid, cplate, price, date_time) " \
               "VALUES (%s, %s, %s, '%s')"
         val = (self.charging_station.id, self.car.plate, self.price, self.date_time)
         cursor.execute(sql, val)
-        db.commit()
+        conn.commit()
         self.id = cursor.lastrowid
 
 
@@ -244,14 +244,14 @@ class RepairRecord:
         self.price = randint(50, 1000)
         self.date_time = get_fake_date_time(fake)
 
-    def save(self, db: MySQLConnection):
-        cursor = db.cursor()
+    def save(self, conn: MySQLConnection):
+        cursor = conn.cursor()
         sql = "INSERT INTO repair_records " \
               "(wid, cplate, price, date_time) " \
               "VALUES (%s, %s, %s, '%s')"
         val = (self.workshop.id, self.car.plate, self.price, self.date_time)
         cursor.execute(sql, val)
-        db.commit()
+        conn.commit()
         self.id = cursor.lastrowid
 
 
@@ -263,14 +263,14 @@ class PaymentRecord:
         self.price = randint(10, 10000)
         self.date_time = get_fake_date_time(fake)
 
-    def save(self, db: MySQLConnection):
-        cursor = db.cursor()
+    def save(self, conn: MySQLConnection):
+        cursor = conn.cursor()
         sql = "INSERT INTO payment_records " \
               "(no_of_transaction, cid, did, price, date_time) " \
               "VALUES (%s, %s, %s, %s, '%s')"
         val = (self.transaction, self.customer.id, self.deposit.id, self.price, self.date_time)
         cursor.execute(sql, val)
-        db.commit()
+        conn.commit()
 
 
 class PlugProperty:
@@ -280,14 +280,14 @@ class PlugProperty:
         self.amount = randint(1, 10)
         _ = fake
 
-    def save(self, db: MySQLConnection):
-        cursor = db.cursor()
+    def save(self, conn: MySQLConnection):
+        cursor = conn.cursor()
         sql = "INSERT INTO plug_properties " \
               "(sid, pmodel, amount) " \
               "VALUES (%s, %s, %s)"
         val = (self.charging_station.id, self.plug.model, self.amount)
         cursor.execute(sql, val)
-        db.commit()
+        conn.commit()
 
 
 class CarPartProperty:
@@ -297,14 +297,14 @@ class CarPartProperty:
         self.amount = randint(0, 50)
         _ = fake
 
-    def save(self, db: MySQLConnection):
-        cursor = db.cursor()
+    def save(self, conn: MySQLConnection):
+        cursor = conn.cursor()
         sql = "INSERT INTO car_part_properties " \
               "(wid, trade_name, pid, amount) " \
               "VALUES (%s, %s, %s, %s)"
         val = (self.workshop.id, self.car_part.trade_name, self.car_part.provider.id, self.amount)
         cursor.execute(sql, val)
-        db.commit()
+        conn.commit()
 
 
 class OrderPayment:
@@ -315,14 +315,14 @@ class OrderPayment:
         self.price = randint(25, 10000)
         self.date_time = get_fake_date_time(fake, start=order.date_time)
 
-    def save(self, db: MySQLConnection):
-        cursor = db.cursor()
+    def save(self, conn: MySQLConnection):
+        cursor = conn.cursor()
         sql = "INSERT INTO order_payment_records " \
               "(no_of_transaction, did, pid, price, date_time) " \
               "VALUES (%s, %s, %s, %s, '%s')"
         val = (self.transaction, self.deposit.id, self.provider.id, self.price, self.date_time)
         cursor.execute(sql, val)
-        db.commit()
+        conn.commit()
 
 
 #############################################################################################
@@ -330,6 +330,6 @@ class OrderPayment:
 
 # for testing purposes
 if __name__ == "__main__":
-    fk = Faker()
+    fk = Faker
     user = Customer(fk)
     print(user.name, user.phone_number)
