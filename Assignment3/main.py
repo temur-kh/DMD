aa926bb2e468c3db0c877c96fa17b2496a964912
Temur <tim_1998@bk.ru>
2018-11-24 01:43:22 +0300

    Create ApplicationWindow object, run tkinter mainloop

--- Assignment3/main.py
+++ Assignment3/main.py
@@ -3,6 +3,8 @@ import mysql.connector
 from create_tables import create_database
 from sample_data.sample_database import SampleDatabase
 from faker import Faker
+import tkinter as tk
+from gui_application.window import ApplicationWindow
 
 conn = mysql.connector.connect(
     host="localhost",
@@ -14,4 +16,12 @@ create_database(conn)
 
 fake = Faker()
 database = SampleDatabase(conn, fake)
-database.upload()
+# database.create_data()  # uncomment if need to create sample data
+# database.upload()       # uncomment if need to upload sample data
+root = tk.Tk()
+root.wm_title("DMD Assignment 3")
+window = ApplicationWindow(root, database)
+window.pack()
+tk.mainloop()
+
+
