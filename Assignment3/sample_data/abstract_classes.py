fc338da5768d3079deb3c2286a0744b13718bc4e
Temur <tim_1998@bk.ru>
2018-11-23 16:49:22 +0300

    Add changes to abstract class Entity

--- Assignment3/sample_data/abstract_classes.py
+++ Assignment3/sample_data/abstract_classes.py
@@ -3,18 +3,10 @@ from mysql.connector import MySQLConnection
 
 
 class Entity(ABC):
-    # @abstractmethod
-    # @property
-    # def primary_keys(self):
-    #     pass
-
-    @abstractmethod
-    def is_unique(self, conn: MySQLConnection):
-        pass
-
     @abstractmethod
     def save(self, conn: MySQLConnection):
         pass
 
-    def __eq__(self, other):
-        return self.__dict__ == other.__dict__
+    @abstractmethod
+    def dublicates(self, other):
+        pass
