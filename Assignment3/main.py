595034a8bdb74223143f3aa0fdc74ef185f06ca9
Temur <tim_1998@bk.ru>
2018-11-23 16:48:47 +0300

    Create sample database and upload its data

--- Assignment3/main.py
+++ Assignment3/main.py
@@ -1,6 +1,8 @@
 import mysql.connector
 
 from create_tables import create_database
+from sample_data.sample_database import SampleDatabase
+from faker import Faker
 
 conn = mysql.connector.connect(
     host="localhost",
@@ -9,3 +11,7 @@ conn = mysql.connector.connect(
 )
 
 create_database(conn)
+
+fake = Faker()
+database = SampleDatabase(conn, fake)
+database.upload()
